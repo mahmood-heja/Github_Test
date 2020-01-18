69b3150237785e7b36350df6131d8ea9cf14503b
mahmood-heja <2014990045@ses.yu.edu.jo>
2020-01-18 13:51:36 +0200

    Initial commit

---
+++ app/src/main/java/com/tasheh/githubtest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.tasheh.githubtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private void test(){
+
+        String test = "test";
+
+    }
+
+}
